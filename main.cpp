a3a69d87676151dd658f942858f220e4a45eb3ca
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-12-11 18:51:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+    int n;
+    float a=0;
+    float max=0;
+    cin >> n;
+    float array[n];
+    cout << valMax(array[n], n);
   return 0;
 }
