9a5a7b6082d472fd012faa11759f137c9fc12c07
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-12-11 19:56:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 float valMax(float array[], int n){
-    float max=0;
-    float a=0;
+    float max = 0;
+    float a = 0;
     for (int i = 0; i < n; ++i) {
         cin >> a;
         array[i] = a;
         if (i==0){
-            max=array[i];
+            max = array[i];
+        }else if (array[i]>max){
+            max = array[i];
         }
-        else if (array[i]>max){
-            max=array[i];
-        }
-    } return max;
+    }
+    return max;
 }
